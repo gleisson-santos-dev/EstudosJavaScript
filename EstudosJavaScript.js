1efd7ffc20f1d5f703916aa476b3dcfbd8ac5481
gleisson_santos <97573446+gleisson-santos-dev@users.noreply.github.com>
2022-02-12 00:47:57 -0400

    Add files via upload

---
+++ EstudosJavaScript.js
@@ -0,0 +1,14 @@
+/*revisão de JavaScript 
+não esquesa de usar o ; e CamelCase  */ 
+
+//variaveis
+let idade = 19;
+var nome = "gleisson";
+//const(constantes)não podem ser auteradas ou declaradas duas veses
+const sobrenome = " santos vieira ";
+ 
+console.log ( idade + " "+nome + sobrenome); 
+/* variaveis declaradas fora de funções são de escopo global
+diponivel para qualquer codigo do ducumennto, variavie locais 
+( declaradas dentro de funções) estão diponiveis apenas para 
+a quelas  funçoes*/
